2dadb6aa788bdf0aa330701de199b3cf67c8e338
Lucy Wilcox <lucy.wilcox@students.olin.edu>
2017-09-29 09:50:28 -0400

    world working

--- code/World.py
+++ code/World.py
@@ -11,20 +11,23 @@ class World(object):
 		add_routes(self.cities)
 		self.infected = infected_ids
 		for city_id in self.infected:
-			self.cities[city_id].infect_self()
+			city = self.cities[city_id]
+			city.infect_self()
+			cities[city_id] = city
+			print(city.infected_timer)
 
 	def should_infect_trade(self, dist):
 		chance_to_infect = 1 - dist/1000
-		return (np.random() < chance_to_infect)
+		return (np.random.random() < chance_to_infect)
 
 	def should_infect_plg(self, dist):
 		chance_to_infect = 1 - dist/1000
-		return (np.random() < chance_to_infect)
+		return (np.random.random() < chance_to_infect)
 
 	def loop(self, n):
 		for _ in range(n):
 			self.step()
-			print(self.infected)
+			print([cities[i].name for i in self.infected])
 
 	def step(self):
 		current_infected = self.infected
@@ -33,15 +36,17 @@ class World(object):
 			for city2_id in city.route_trade:
 				city2 = cities[city2_id]
 				if not city2.is_infected:
-					if should_infect_trade(city.route_trade[city2_id]):
+					if self.should_infect_trade(city.route_trade[city2_id]):
 						city2.infect_self()
+						cities[city2_id] = city2
 						self.infected.append(city2_id)
 
 			for city2_id in city.route_plg:
 				city2 = cities[city2_id]
 				if not city2.is_infected:
-					if should_infect_plg(city.route_plg[city2_id]):
+					if self.should_infect_plg(city.route_plg[city2_id]):
 						city2.infect_self()
+						cities[city2_id] = city2
 						self.infected.append(city2_id)
 
 		for city_id in self.infected:
@@ -51,4 +56,4 @@ class World(object):
 				self.infected.remove(city_id)
 
 world = World([2582, 2580, 219, 1630, 855])
-world.loop(20)
+world.loop(50)
